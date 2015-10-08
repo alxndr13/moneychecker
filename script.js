25a8751a9eb5fb29083184a24a10bd7198c02b51
Alexander Huck <alexhuck01@gmail.com>
2015-10-08 11:00:06 +0200

    Version 1 - bereit für CSS Implementierung

---
+++ script.js
@@ -0,0 +1,173 @@
+function calculate() {
+    var z1 = document.getElementById("z1").value;
+    var z2 = document.getElementById("z1").value;
+
+    var w1 = document.getElementById("w1").value;
+    var w2 = document.getElementById("w2").value;
+    var erg;
+    
+    //Dollar
+    if (w1 == "Dollar" && w2 == "Dollar") {
+        alert("Bitte wähle eine andere Zielwährung aus!");
+    }
+    if (w1 == "Dollar" && w2 == "Euro") {
+        DollarInEuro(z1);
+    }
+    if (w1 == "Dollar" && w2 == "Pfund") {
+        DollarInPfund(z1);
+    }
+    if (w1 == "Dollar" && w2 == "Rubel") {
+        DollarInRubel(z1);
+    }
+    if (w1 == "Dollar" && w2 == "Yen") {
+        DollarInYen(z1);
+    }
+    //Euro
+    if (w1 == "Euro" && w2 == "Euro") {
+        alert("Bitte wähle eine andere Zielwährung aus!");
+    }
+    if (w1 == "Euro" && w2 == "Dollar") {
+        EuroInDollar(z1);
+    }
+    if (w1 == "Euro" && w2 == "Rubel") {
+        EuroInRubel(z1);
+    }
+    if (w1 == "Euro" && w2 == "Pfund") {
+        EuroInPfund(z1);
+    }
+    if (w1 == "Euro" && w2 == "Yen") {
+        EuroInYen(z1);
+    }
+    //Rubel
+    if (w1 == "Rubel" && w2 == "Rubel") {
+        alert("Bitte wähle eine andere Zielwährung aus!");
+    }
+    if (w1 == "Rubel" && w2 == "Dollar") {
+        RubelInDollar(z1);
+    }
+    if (w1 == "Rubel" && w2 == "Euro") {
+        RubelInEuro(z1);
+    }
+    if (w1 == "Rubel" && w2 == "Pfund") {
+        RubelInPfund(z1);
+    }
+    if (w1 == "Rubel" && w2 == "Yen") {
+        RubelInYen(z1);
+    }
+    if (w1 == "Yen" && w2 == "Yen") {
+      alert("Bitte wähle eine andere Zielwährung aus!");
+    }
+    if (w1 == "Yen" && w2 == "Dollar") {
+        YenInDollar(z1);
+    }
+    if (w1 == "Yen" && w2 == "Euro") {
+        YenInEuro(z1);
+    }
+    if (w1 == "Yen" && w2 == "Pfund") {
+        YenInPfund(z1);
+    }
+    if (w1 == "Yen" && w2 == "Rubel") {
+        YenInRubel(z1);
+    }
+    if (w1 == "Pfund" && w2 == "Pfund") {
+      alert("Bitte wähle eine andere Zielwährung aus!");
+    }
+    if (w1 == "Pfund" && w2 == "Dollar") {
+        PfundInDollar(z1);
+    }
+    if (w1 == "Pfund" && w2 == "Euro") {
+        PfundInEuro(z1);
+    }
+    if (w1 == "Pfund" && w2 == "Rubel") {
+        PfundInRubel(z1);
+    }
+    if (w1 == "Pfund" && w2 == "Yen") {
+        PfundInYen(z1);
+    }
+    
+}
+
+function DollarInEuro(wert) {
+    erg = wert  * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function DollarInPfund(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function DollarInRubel(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function DollarInYen(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function EuroInDollar(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function EuroInRubel(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function EuroInYen(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function EuroInPfund(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function RubelInDollar(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function RubelInEuro(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function RubelInYen(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function RubelInPfund(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function YenInDollar(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function YenInEuro(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function YenInRubel(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function YenInPfund(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function PfundInDollar(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function PfundInEuro(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function PfundInRubel(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function PfundInYen(wert) {
+    erg = wert * 1.5;
+    document.getElementById("ergebnis").innerHTML = erg;
+}
+function clearall() {
+    document.getElementById("ergebnis").innerHTML = "";
+    document.getElementById("z1").value = "";
+}
