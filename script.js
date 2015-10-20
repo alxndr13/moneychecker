78a7b41f7e868bfc759d2676f16086fe9a7ebe94
mrnhrst <marian@Marians-Mini.fritz.box>
2015-10-20 21:04:53 +0200

    SVG eingebunden und JS-Datei mit Funktionen für SVG erweitert.
    Größe vom SVG muss noch angepasst werden

--- script.js
+++ script.js
@@ -9,80 +9,130 @@ function calculate() {
     //Dollar
     if (w1 == "Dollar" && w2 == "Dollar") {
         alert("Bitte wähle eine andere Zielwährung aus!");
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "220");
     }
     if (w1 == "Dollar" && w2 == "Euro") {
         DollarInEuro(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "193.43");
     }
     if (w1 == "Dollar" && w2 == "Pfund") {
         DollarInPfund(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "143.93");
     }
     if (w1 == "Dollar" && w2 == "Rubel") {
         DollarInRubel(z1);
+        document.getElementById("rect1").setAttribute("height", "5.03");
+        document.getElementById("rect2").setAttribute("height", "330");
     }
     if (w1 == "Dollar" && w2 == "Yen") {
         DollarInYen(z1);
+        document.getElementById("rect1").setAttribute("height", "2.75");
+        document.getElementById("rect2").setAttribute("height", "330");
     }
     //Euro
     if (w1 == "Euro" && w2 == "Euro") {
         alert("Bitte wähle eine andere Zielwährung aus!");
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", ".220");
     }
     if (w1 == "Euro" && w2 == "Dollar") {
         EuroInDollar(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "250.19");
     }
     if (w1 == "Euro" && w2 == "Rubel") {
         EuroInRubel(z1);
+        document.getElementById("rect1").setAttribute("height", "330");
+        document.getElementById("rect2").setAttribute("height", "4.64");
     }
     if (w1 == "Euro" && w2 == "Pfund") {
         EuroInPfund(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "163.69");
     }
     if (w1 == "Euro" && w2 == "Yen") {
         EuroInYen(z1);
+        document.getElementById("rect1").setAttribute("height", "2.42");
+        document.getElementById("rect2").setAttribute("height", "330");
     }
     //Rubel
     if (w1 == "Rubel" && w2 == "Rubel") {
         alert("Bitte wähle eine andere Zielwährung aus!");
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "220");
     }
     if (w1 == "Rubel" && w2 == "Dollar") {
         RubelInDollar(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "3.51");
     }
     if (w1 == "Rubel" && w2 == "Euro") {
         RubelInEuro(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "3.09");
     }
     if (w1 == "Rubel" && w2 == "Pfund") {
         RubelInPfund(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "2.3");
     }
     if (w1 == "Rubel" && w2 == "Yen") {
         RubelInYen(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "420.2");
     }
     if (w1 == "Yen" && w2 == "Yen") {
         alert("Bitte wähle eine andere Zielwährung aus!");
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "220");
     }
     if (w1 == "Yen" && w2 == "Dollar") {
         YenInDollar(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "1.84");
     }
     if (w1 == "Yen" && w2 == "Euro") {
         YenInEuro(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "1.6");
     }
     if (w1 == "Yen" && w2 == "Pfund") {
         YenInPfund(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "1.2");
     }
     if (w1 == "Yen" && w2 == "Rubel") {
         YenInRubel(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "114.88");
     }
     if (w1 == "Pfund" && w2 == "Pfund") {
         alert("Bitte wähle eine andere Zielwährung aus!");
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "220");
     }
     if (w1 == "Pfund" && w2 == "Dollar") {
         PfundInDollar(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "336,22");
     }
     if (w1 == "Pfund" && w2 == "Euro") {
         PfundInEuro(z1);
+        document.getElementById("rect1").setAttribute("height", "220");
+        document.getElementById("rect2").setAttribute("height", "295.61");
     }
     if (w1 == "Pfund" && w2 == "Rubel") {
         PfundInRubel(z1);
+        document.getElementById("rect1").setAttribute("height", "3.45");
+        document.getElementById("rect2").setAttribute("height", "330");
     }
     if (w1 == "Pfund" && w2 == "Yen") {
         PfundInYen(z1);
+        document.getElementById("rect1").setAttribute("height", "18.02");
+        document.getElementById("rect2").setAttribute("height", "330");
     }
 
 }
